860f7cbbde630a0b77876d18000e76431eb4fb15
EDB\ahmed.tagalsir <ahmed.alhassen.tagalsir@gmail.com>
2022-02-16 11:08:16 +0200

    ssl

--- src/main/java/com/creditApi/creditapi/service/CreditAPI.java
+++ src/main/java/com/creditApi/creditapi/service/CreditAPI.java
@@ -24,20 +24,20 @@ public class CreditAPI {
 
      @Value("${baseurl}")
     private String uri ;
-//    static
-//    {
-//
-//        System.setProperty("javax.net.ssl.keyStore",  "Corporate_Keystore.jks.old");
-//        System.setProperty("javax.net.ssl.keyStorePassword", "nexttech");
-//        javax.net.ssl.HttpsURLConnection.setDefaultHostnameVerifier(
-//                new javax.net.ssl.HostnameVerifier(){
-//
-//                    public boolean verify(String hostname,
-//                                          javax.net.ssl.SSLSession sslSession) {
-//                        return true;
-//                    }
-//                });
-//    }
+    static
+    {
+
+        System.setProperty("javax.net.ssl.keyStore",  "Corporate_Keystore.jks.old");
+        System.setProperty("javax.net.ssl.keyStorePassword", "nexttech");
+        javax.net.ssl.HttpsURLConnection.setDefaultHostnameVerifier(
+                new javax.net.ssl.HostnameVerifier(){
+
+                    public boolean verify(String hostname,
+                                          javax.net.ssl.SSLSession sslSession) {
+                        return true;
+                    }
+                });
+    }
 
     public CreditAPI() {
         restTemplate.setInterceptors(Collections.singletonList(new RequestResponseLoggingInterceptor()));
